353f1e8517f613817d35d694e812a0a8f3aade23
Dan Granville <dan@data-futures.org>
2024-06-10 11:58:28 +0100

    InvenioRDMAdapter

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,24 +1,77 @@
 
 import mirador from 'mirador/dist/es/src/index';
 import annotationPlugins from '../../src';
-import LocalStorageAdapter from '../../src/LocalStorageAdapter';
-import AnnototAdapter from '../../src/AnnototAdapter';
+import InvenioRDMAdapter from '../../src/InvenioRDMAdapter';
 
-const endpointUrl = 'http://127.0.0.1:3000/annotations';
-const config = {
-  annotation: {
-    adapter: (canvasId) => new LocalStorageAdapter(`localStorage://?canvasId=${canvasId}`),
-    // adapter: (canvasId) => new AnnototAdapter(canvasId, endpointUrl),
-    exportLocalStorageAnnotations: false, // display annotation JSON export button
-  },
-  id: 'demo',
-  window: {
-    defaultSideBarPanel: 'annotations',
-    sideBarOpenByDefault: true,
-  },
-  windows: [{
-    loadedManifest: 'https://iiif.harvardartmuseums.org/manifests/object/299843',
-  }],
-};
+function initMirador({ id }) {
+  const params = new URLSearchParams(location.search);
+  var manifest = params.get('m');
+  var canvas = params.get('c');
 
-mirador.viewer(config, [...annotationPlugins]);
+  const el = document.getElementById(id);
+  let config = {}, annotations = {};
+
+  if (el) {
+    manifest = el.dataset.manifest;
+    canvas = el.dataset.canvas;
+    config = JSON.parse(el.dataset.config || '{}');
+    annotations = JSON.parse(el.dataset.annotations || '{}');
+  } else {
+    console.error('Manifest and canvas not provided.');
+    return;
+  }
+
+  const defaultConfig = {
+    annotation: {
+      adapter: (canvasId) => new InvenioRDMAdapter(canvasId),
+      exportLocalStorageAnnotations: false, 
+    },
+    workspaceControlPanel: {
+      enabled: false,
+    },
+    window: {
+      allowClose: false,
+      allowFullscreen: true,
+      allowTopMenuButton: true,
+      allowMaximize: false,
+      allowWindowSideBar: true,
+      defaultSideBarPanel: 'annotations',
+      defaultSidebarPanelHeight: 201,
+      defaultSidebarPanelWidth: 235,
+      defaultView: 'single',
+      hideWindowTitle: true,
+      sideBarOpen: false,
+      switchCanvasOnSearch: false,
+      panels: {
+        info: true,
+        attribution: true,
+        canvas: false,
+        annotations: false,
+        search: false,
+        layers: false,
+      },
+      views: [
+        { key: 'single', behaviors: ['individuals'] },
+        { key: 'book', behaviors: ['paged'] },
+        { key: 'scroll', behaviors: ['continuous'] },
+        { key: 'gallery' },
+      ],
+    },
+  };
+
+  const mergedConfig = {
+    id: id,
+    windows: [{
+      manifestId: manifest,
+      canvasId: canvas,
+      thumbnailNavigationPosition: "far-bottom",
+    }],
+    ...defaultConfig,
+    ...config,
+  };
+  Mirador.viewer(mergedConfig, [...annotationPlugins]);
+}
+
+initMirador({
+  id: 'm3-dist',
+});
---
+++ src/InvenioRDMAdapter.js
@@ -0,0 +1,25 @@
+/** */
+export default class InvenioRDMAdapter {
+  /** */
+  constructor(canvasId) {
+    this.canvasId = canvasId;
+  }
+
+  /** */
+  get annotationPageId() {
+    // assume for now that canvas.jpg has an associated canvas.jpg.short.wadm 
+    // ideally have an endpoint in /api/iiif which is more flexible
+
+    let url = this.canvasId
+      .replace("iiif/record:", "record/")
+      .replace("canvas", "files");
+
+    url = `${url}short.wadm/content`;
+    return url;
+  }
+
+  /** */
+  async all() {
+    return (await fetch(this.annotationPageId)).json();
+  }
+}
